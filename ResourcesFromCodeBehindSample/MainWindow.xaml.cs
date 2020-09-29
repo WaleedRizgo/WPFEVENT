ad49da823b0bc5d5efe310ce63dd7ace1be90b01
Waleed Rizgo <waleedrizgo@gmail.com>
2020-09-29 16:15:10 +0200

    29.09.2020 UM 16:15

---
+++ ResourcesFromCodeBehindSample/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace ResourcesFromCodeBehindSample.WPF_Application
+{
+    public partial class ResourcesFromCodeBehindSample : Window
+    {
+        public ResourcesFromCodeBehindSample()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            lbResult.Items.Add(pnlMain.FindResource("strPanel").ToString());
+            lbResult.Items.Add(this.FindResource("strWindow").ToString());
+            lbResult.Items.Add(Application.Current.FindResource("strApp").ToString());
+        }
+    }
+}
